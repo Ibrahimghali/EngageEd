730990be2fb254d464d34d2d7ff3caaf43927f10
Ibrahimghali <ghaliibrahim60@gmail.com>
2025-03-16 06:31:05 +0100

    first commit

---
+++ EngageEd/src/test/java/com/EngageEd/EngageEd/EngageEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EngageEd.EngageEd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngageEdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
